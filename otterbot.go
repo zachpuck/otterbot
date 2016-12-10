218459bbf81deb02d2c501754b4144a354c3385d
zachpuck <zachpuckett@gmail.com>
2016-12-09 19:12:35 -0800

    return google geolocation details for montereybay2 as string

--- otterbot.go
+++ otterbot.go
@@ -26,8 +26,10 @@ THE SOFTWARE.
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"strconv"
 	"strings"
@@ -70,26 +72,6 @@ func main() {
 	}
 }
 
-// Get the quote via Yahoo. You should replace this method to something
-// relevant to your team!
-/*func getInfo(sym string) string {
-	sym = strings.ToUpper(sym)
-	url := fmt.Sprintf("http://download.finance.yahoo.com/d/quotes.csv?s=%s&f=nsl1op&e=.csv", sym)
-	resp, err := http.Get(url)
-	if err != nil {
-		return fmt.Sprintf("error: %v", err)
-	}
-	rows, err := csv.NewReader(resp.Body).ReadAll()
-	if err != nil {
-		return fmt.Sprintf("error: %v", err)
-	}
-	if len(rows) >= 1 && len(rows[0]) == 5 {
-		return fmt.Sprintf("%s (%s) is trading at $%s", rows[0][0], rows[0][1], rows[0][2])
-	}
-	return fmt.Sprintf("unknown response format (symbol was \"%s\")", sym)
-}
-*/
-
 // getting info from: http://www.otter-world.com/sea-otter/
 func getInfo(topic string) string {
 	checkTopics := strings.ToLower(topic)
@@ -109,11 +91,105 @@ func getInfo(topic string) string {
 	case "montereybay":
 		loc := coordinates{36.8007, 121.9473}
 		return "Monterey Bay is located at " + strconv.FormatFloat(loc.n, 'f', -1, 64) + "° N, " + strconv.FormatFloat(loc.w, 'f', -1, 64) + "° W"
+	case "montereybay2":
+		location := getCoordinates("montereybay")
+		return location
 	default:
-		return "current options are: \"info types\" or \"info SeaOtter\" or \"MontereyBay\"!"
+		return "current options are: \"info types\" or \"info SeaOtter\" or \"MontereyBay\"! or \"MontereyBay2\""
 	}
 }
 
 type coordinates struct {
 	n, w float64
+}
+
+// as a user of otterbot i can type a city name and be returned coordinates
+// calling on google api to return the location
+//https://medium.com/@IndianGuru/consuming-json-apis-with-go-d711efc1dcf9#.23opkpqgw
+
+func getCoordinates(address string) string {
+	url := fmt.Sprintf("https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=<api key>", address)
+
+	// Build the request
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal("NewRequest: ", err)
+		return "something bad happened in the build request URL"
+	}
+
+	// For control over HTTP client headers,
+	// redirect policy, and other settings,
+	// create a Client
+	// A Client is an HTTP client
+	client := &http.Client{}
+
+	// Send the request via a client
+	// Do sends an HTTP request and
+	// returns an HTTP response
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Do: ", err)
+		return "something bad happened in the do http request"
+	}
+
+	// Callers should close resp.Body
+	// when done reading from it
+	// Defer the closing of the body
+	defer resp.Body.Close()
+
+	// Fill the record with the data from the JSON
+	var record GeoLoc
+
+	// Use json.Decode for reading streams of JSON data
+	if err := json.NewDecoder(resp.Body).Decode(&record); err != nil {
+		log.Println(err)
+	}
+
+	// fmt.Println(record)
+
+	details := fmt.Sprintf("%#v", record.Results)
+	return details
+}
+
+// google returns the following data.
+type GeoLoc struct {
+	Results []struct {
+		AddressComponents []struct {
+			LongName  string   `json:"long_name"`
+			ShortName string   `json:"short_name"`
+			Types     []string `json:"types"`
+		} `json:"address_components"`
+		FormattedAddress string `json:"formatted_address"`
+		Geometry         struct {
+			Bounds struct {
+				Northeast struct {
+					Lat float64 `json:"lat"`
+					Lng float64 `json:"lng"`
+				} `json:"northeast"`
+				Southwest struct {
+					Lat float64 `json:"lat"`
+					Lng float64 `json:"lng"`
+				} `json:"southwest"`
+			} `json:"bounds"`
+			Location struct {
+				Lat float64 `json:"lat"`
+				Lng float64 `json:"lng"`
+			} `json:"location"`
+			LocationType string `json:"location_type"`
+			Viewport     struct {
+				Northeast struct {
+					Lat float64 `json:"lat"`
+					Lng float64 `json:"lng"`
+				} `json:"northeast"`
+				Southwest struct {
+					Lat float64 `json:"lat"`
+					Lng float64 `json:"lng"`
+				} `json:"southwest"`
+			} `json:"viewport"`
+		} `json:"geometry"`
+		PartialMatch bool     `json:"partial_match"`
+		PlaceID      string   `json:"place_id"`
+		Types        []string `json:"types"`
+	} `json:"results"`
+	Status string `json:"status"`
 }
